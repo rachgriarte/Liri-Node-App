f4861459547d1663dca69731f5c345ff59142694
Rach Griarte <rachelgriarte@hotmail.com>
2017-07-19 15:13:43 -0400

    adding files

---
+++ assets/js/keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'ZXCyDvHXfYZdr5iqtqJ6xzQJZ',
+  consumer_secret: 'MzYytuSarOWk3IyH7ZshCFRfGJyaDwby579pN18yQk9jP7pSMy',
+  access_token_key: '77833717-LBtsXNgA71ffu8fqGiHjkEFjpLABEDDctkIBU1RAf',
+  access_token_secret: 'BdXyTZ8NV6rTcVRUkolGiUxwOCXao9gaCbeq91Mb3GTmA',
+}

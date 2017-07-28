c52d0c4f03465e47d041dc9a2236503c94a033e0
Rach Griarte <rachelgriarte@hotmail.com>
2017-07-27 21:31:03 -0400

    last push, hopefullygit add .

--- keys.js
+++ keys.js
@@ -3,13 +3,13 @@ exports.twitterKeys = {
   consumer_key: 'ZXCyDvHXfYZdr5iqtqJ6xzQJZ',
   consumer_secret: 'MzYytuSarOWk3IyH7ZshCFRfGJyaDwby579pN18yQk9jP7pSMy',
   access_token_key: '77833717-LBtsXNgA71ffu8fqGiHjkEFjpLABEDDctkIBU1RAf',
-  access_token_secret: 'BdXyTZ8NV6rTcVRUkolGiUxwOCXao9gaCbeq91Mb3GTmA',
+  access_token_secret: 'BdXyTZ8NV6rTcVRUkolGiUxwOCXao9gaCbeq91Mb3GTmA'
 };
 
 // Pulling information from Spotify
 exports.spotifyKeys = {
-	Client_ID: '3ff2211120454ab694caa87c4bd70add',
-	Client_Secret: '43950262e0d84ac9afcfb053ff54c6e4',
+	id: '3ff2211120454ab694caa87c4bd70add',
+	secret: '43950262e0d84ac9afcfb053ff54c6e4'
 };
 
 
--- liri.js
+++ liri.js
@@ -1,5 +1,13 @@
 //Introduction
-console.log("Hi, welcome to Liri!");
+console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+console.log("\nHi Stephen, welcome to Liri!");
+console.log("\nSome helpful tips:");
+console.log("To see your tweets, use 'my-tweets' as your third node argument");
+console.log("To learn more information about a song, use 'spotify-this-song' as your third node argument");
+console.log("To learn more information about a movie, use 'movie-this' as your third node argument");
+console.log("Use 'do-what-it-says' as your third node argument and see what it does!");
+console.log("\nHAVE FUN! :)");
+console.log("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 // Grabs the Twitter & Spotify API Key
 var theKeys = require ("./keys.js");
 //Grabbing the File Set 
@@ -36,33 +44,35 @@ function tweets(){
 	client.get('statuses/user_timeline', params, function(error, tweets, response) {
 	  if (!error) {
 	  	for (var i=0; i<tweets.length; i++){
+	  		console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	  		console.log(" ");
 	  		console.log(tweets[i].created_at)
 			console.log(tweets[i].text);
-			console.log("------------------------------------------------------");
+			console.log("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 	   		}
 	   	}
 	});
 }
 //---------- If song function is called ----------
 function song() {
-	var pullSong = new Spotify(theKeys.spotifyKeys);
+	var client = new Spotify(theKeys.spotifyKeys);
 	var playSong = process.argv[3];
 	//retrieving Spotify song
-	pullSong.search({ type: 'track', query: playSong, limit: 1}, function(error, data) {
-  		if (err) {
-    		return console.log('Error occurred: ' + err);
+	client.search({ type: 'track', query: playSong, limit: 1}, function(error, data) {
+  		if (error) {
+    		return console.log('Error occurred: ' + error);
   			} else {
 				var artistNames = function (artist) {  
 				return artist.name;				
   			}
   			var tracks=data.tracks.items;
   			for (var i=0; i<tracks.length; i++){
-			console.log('-----------------------------------------------------------');
-	    	console.log('Artist(s): ' + tracks[i].artists.map(artistNames));
-	    	console.log('song name: ' + tracks[i].name);
-	    	console.log('preview song: ' + tracks[i].preview_url);
-	    	console.log('album: ' + tracks[i].album.name);
-	    	console.log('-----------------------------------------------------------');
+			console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+	    	console.log('\nArtist(s): ' + tracks[i].artists.map(artistNames));
+	    	console.log('Song Name: ' + tracks[i].name);
+	    	console.log('Preview Song: ' + tracks[i].preview_url);
+	    	console.log('Album: ' + tracks[i].album.name);
+	    	console.log("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 			}	
 		}
 	});
@@ -71,21 +81,21 @@ function song() {
 function movie() {
 	// Then run a request to the OMDB API with the movie specified
 	var queryURL = "http://www.omdbapi.com/?t=" + command2 + "&y=&plot=short&apikey=40e9cece";
-	// This line will help us debug against the actual queryURL
-	console.log(queryURL);
-
 	request(queryURL, function (error, response, body) {
 		// if the request is successful, run this
 		if (!error && response.statusCode === 200) {
-			console.log('-----------------------------------------------------------');
+			console.log("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+			console.log(" ");
+			console.log(queryURL);
+			console.log("\nMovie Title: " +command2);
+			console.log("Actors in the movie: " + JSON.parse(body).Actors);
 			console.log("Release Year: " + JSON.parse(body).Year);
-			console.log("The IMDB rating is: " + JSON.parse(body).imdbRating);
-			console.log("Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[2].Value);
 			console.log("Country where the movie is produced: " + JSON.parse(body).Country);
 			console.log("Language of the movie: " + JSON.parse(body).Language);
-			console.log("Plot of the movie: " + JSON.parse(body).Plot);
-			console.log("Actors in the movie: " + JSON.parse(body).Actors);
-			console.log('-----------------------------------------------------------');
+			console.log("\nPlot of the movie: " + JSON.parse(body).Plot);
+			console.log("\nThe IMDB rating is: " + JSON.parse(body).imdbRating);
+			console.log("Rotten Tomatoes rating is: " + JSON.parse(body).Ratings[2].Value);
+			console.log("\nXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
 		}
 	});
 }
